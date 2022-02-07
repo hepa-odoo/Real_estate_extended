2c04d90ee7761d38e5516fa211c78648c9331efd
Het Patel <hetpatel@Hets-MacBook-Air.local>
2022-02-07 11:11:06 +0530

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+   'name': 'Real Estate Extended',
+   'version': '1.0',
+   'description': "",
+   'author':"Het Patel",
+   'depends': ['base','Real_estate'],
+   'data' : [
+       'security/ir.model.access.csv',
+       'views/extended_view.xml',
+   ],
+   'installable': True,
+   'auto_install': False,
+   'application': True,
+   'license': 'LGPL-3',
+}
